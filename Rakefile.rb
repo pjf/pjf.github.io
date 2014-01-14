98fa7fdff953efb9414af706a9ff0945bbc923ba
Paul Fenwick <pjf@perltraining.com.au>
2014-01-15 04:13:48 +1100

    Publish from rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,7 +1,13 @@
+# Based upon the code at
+# http://ixti.net/software/2013/01/28/using-jekyll-plugins-on-github-pages.html
+
 require 'rubygems'
 require 'tmpdir'
 require 'bundler/setup'
 require 'jekyll'
+
+# TODO: auto-detect this
+GITHUB_REPONAME = 'pjf/pjf.github.io'
 
 desc "Generate site"
 task :default do
@@ -11,4 +17,21 @@ task :default do
     })).process
 end
 
+desc "Generate and publish"
+task :publish => [:default] do
+  Dir.mktmpdir do |tmp|
+    cp_r "_site/.", tmp
 
+    pwd = Dir.pwd
+    Dir.chdir tmp
+
+    system "git init"
+    system "git add ."
+    message = "Site updated at #{Time.now.utc}"
+    system "git commit -m #{message.inspect}"
+    system "git remote add origin git@github.com:#{GITHUB_REPONAME}.git"
+    system "git push origin master --force"
+
+    Dir.chdir pwd
+  end
+end
