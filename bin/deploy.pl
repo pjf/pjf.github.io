0c0f835e711f3449011fd144cfced4c064c3e8b2
Paul Fenwick <pjf@perltraining.com.au>
2014-01-15 19:05:00 +1100

    Quick and dirty deploy script that sucks less.

---
+++ bin/deploy.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+use 5.010;
+use strict;
+use warnings;
+use autodie qw(:all);
+use File::Path qw(remove_tree);
+use FindBin qw($Bin);
+
+chdir("$Bin/..");
+
+system("jekyll");
+system("git checkout master");
+
+# Make a hash of all our (non-dot) files
+my %files;
+@files{ glob("*") } = ();
+
+# Remove _site from that list
+delete $files{_site};
+
+# But get rid of all the rest.
+remove_tree(keys %files);
+
+# Move everything in site up a level
+system("mv _site/* .");
+
+# And send to githb.
+system("git add -A");
+system("git push");
+
+# Finally, return home
+system("git checkout build");
