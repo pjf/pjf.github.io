0282c4cfd77983e7b1fcfa72343208020246e482
Paul Fenwick <pjf@perltraining.com.au>
2014-01-16 16:01:22 +1100

    Make youtube videos responsive.

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -27,7 +27,7 @@ class YouTube < Liquid::Tag
   end
 
   def render(context)
-    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?html5=1\"> </iframe><p><a href=\"http://www.youtube.com/watch?v=#{@id}\">Watch on YouTube</a></p>"
+    "<div class=\"video-container\"><iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?html5=1\"> </iframe></div><p><a href=\"http://www.youtube.com/watch?v=#{@id}\">Watch on YouTube</a></p>"
   end
 
   Liquid::Template.register_tag "youtube", self
