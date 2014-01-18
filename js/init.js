874cdef55c78a2025a8c312e4d024772727830e1
Paul Fenwick <pjf@perltraining.com.au>
2014-01-18 21:32:44 +1100

    Site update at Sat Jan 18 21:32:44 2014

--- js/init.js
+++ js/init.js
@@ -76,20 +76,24 @@ skel.init(
 
 $(function() {
 
-	var $window = $(window), $document = $(document), $sc = $('#sidebar, #content'), tid;
-	
-	$window.load(function() {
+    var $window = $(window), $document = $(document), $sc = $('#sidebar, #content'), tid;
+    
+    $window.load(function() {
 
-		$window.resize(function() {
-			window.clearTimeout(tid);
-			tid = window.setTimeout(function() {
-				if (skel.isActive('mobile') || skel.isActive('narrower'))
-					$sc.css('min-height', '0').css('height', 'auto');
-				else
-					$sc.css('min-height', $window.height()).css('height', $document.height());
-			}, 100);
-		}).trigger('resize');
-		
-	});
+        $window.resize(function() {
+            window.clearTimeout(tid);
+            tid = window.setTimeout(function() {
+                var $sc = $('#sidebar, #content');
+
+if (skel.isActive('mobile') || skel.isActive('narrower'))
+    $sc.css('min-height', '0').css('height', 'auto');
+else
+    $sc.css('min-height', $(window).height()).css('height', $(document).height());
+
+
+            }, 100);
+        }).trigger('resize');
+        
+    });
 
 });
